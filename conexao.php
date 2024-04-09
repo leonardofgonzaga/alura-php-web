1e6f17eb70630c240f9c9d9d007f92c617043d6a
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-04-09 19:51:39 -0300

    PHP e PDO: executando SQL

--- conexao.php
+++ conexao.php
@@ -1,6 +1,10 @@
 <?php 
 
-$caminhoBanco = __DIR__ ."/banco.sqlite";
-$pdo = new PDO('sqlite:'.$caminhoBanco);
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ ."/banco.sqlite";
+$pdo = new PDO('sqlite:'.$databasePath);
 
 echo "Conectei";
+
+$pdo->exec("CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT)");
