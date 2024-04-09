83af267d6df118a1043854eb0cf4c65e972783c5
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-04-09 20:14:16 -0300

    PHP e PDO: modos de busca

--- lista-alunos.php
+++ lista-alunos.php
@@ -1,9 +1,24 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . "/banco.sqlite";
 $pdo = new PDO('sqlite:' . $databasePath);
 
 $stmt = $pdo->query('SELECT * FROM students;');
-var_dump($stmt->fetchAll());
+$studentDataList = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($stmt->fetchAll(PDO::FETCH_CLASS, Student::class));
+
+$studenList = [];
+foreach ($studentDataList as $studentData) {
+ 
+    $studenList[] = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new DateTimeImmutable($studentData['birth_Date'])
+    );
+}
+
+var_dump($studenList);
