625173887218dba8a7a975dd53a6d47d84445921
Leonardo Gonzaga <leonardo.fg95@gmail.com>
2024-04-10 20:10:31 -0300

    PHP e PDO: prepared statements

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -7,8 +7,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . "/banco.sqlite";
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Leonardo Gonzaga', new DateTimeImmutable('1995-03-03'));
+$student = new Student(
+    null, 
+    "Leonardo', ''); DROP TABLE students; -- Gonzaga", 
+    new DateTimeImmutable('1995-03-03')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date)";
+$stmt = $pdo->prepare($sqlInsert);
+$stmt->bindValue("name", $student->name());
+$stmt->bindValue("birth_date", $student->birthDate()->format("Y-m-d"));
 
-var_dump($pdo->exec($sqlInsert));
+if($stmt->execute()) {
+    echo "Aluno incluído";
+}
